01e9ebfec2ffa887f89489db6ac03214e9805a89
Gabe Rubio <grubio@arrow.com>
2017-02-13 23:06:52 -0700

    renaming files. touching up spacing. adding scroll logic to google maps

--- projects/West-Edison/js/popUp.js
+++ projects/West-Edison/js/popUp.js
@@ -1,63 +1,51 @@
 $(document).ready(function() {
-
-    //if the cookie hasLaunch is not set, then show the modal window
+    //If the cookie hasLaunch is not set, then show the modal window
     if (!readCookie('hasLaunch')) {
-        //launch it
-        launchWindow('#dialog');
+      launchWindow('#dialog');
     }
     else {
       allowScroll();
     }
-
-    //if yes button is clicked
+    // If yes button is clicked, set cookie
     $('.window #yes').click(function () {
-        $('#mask').hide();
-        $('.window').hide();
-        allowScroll();
-        //then set the cookie, so next time the modal won't be displaying again.
-        createCookie('hasLaunch', 1, 365);
+      $('#mask').hide();
+      $('.window').hide();
+      allowScroll();
+      createCookie('hasLaunch', 1, 365);
     });
-
+    // If no, redirect to cat videos
     $('.window #no').click(function() {
       window.location.href = "https://www.youtube.com/watch?v=REVeqcS2aEU";
     });
-
     $(window).resize(function() {
       setModal();
     });
-
 });
 
 function allowScroll() {
-  $('body').removeClass('no-scroll');
-  $('html').removeClass('no-scroll');
+  $('body , html').removeClass('no-scroll');
 }
 
 function setModal() {
-
-      var box = $('#boxes .window');
-
-      //Get the screen height and width
-      var maskHeight = $(document).height();
-      var maskWidth = $(window).width();
-
-      //Set height and width to mask to fill up the whole screen
-      $('#mask').css({'width':maskWidth,'height':maskHeight});
-
-      //Get the window height and width
-      var winH = $(window).height();
-      var winW = $(window).width();
-
-      //Set the popup window to center
-      box.css('top',  winH/2 - box.height()/2);
-      box.css('left', winW/2 - box.width()/2);
+  var box = $('#boxes .window');
+  //Get the screen height and width
+  var maskHeight = $(document).height();
+  var maskWidth = $(window).width();
+  //Set height and width to mask to fill up the whole screen
+  $('#mask').css({'width':maskWidth,'height':maskHeight});
+  //Get the window height and width
+  var winH = $(window).height();
+  var winW = $(window).width();
+  //Set the popup window to center
+  box.css('top',  winH/2 - box.height()/2);
+  box.css('left', winW/2 - box.width()/2);
 }
 
 function createCookie(name,value,days) {
     if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
+      var date = new Date();
+      date.setTime(date.getTime()+(days*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
     }
     else {
       var expires = "";
@@ -69,34 +57,28 @@ function readCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+      var c = ca[i];
+      while (c.charAt(0)==' ') c = c.substring(1,c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
     }
     return null;
 }
 
 function launchWindow(id) {
-
         //Get the screen height and width
         var maskHeight = $(document).height();
         var maskWidth = $(window).width();
-
         //Set heigth and width to mask to fill up the whole screen
         $('#mask').css({'width':maskWidth,'height':maskHeight});
-
         //transition effect
         $('#mask').fadeIn(1000);
         $('#mask').fadeTo("slow",0.8);
-
         //Get the window height and width
         var winH = $(window).height();
         var winW = $(window).width();
-
         //Set the popup window to center
         $(id).css('top',  winH/2-$(id).height());
         $(id).css('left', winW/2-$(id).width()/2);
-
         //transition effect
         $(id).fadeIn(2000);
 }
